b117033fb5b1198ab56cd31c99ec1c798c343886
Roman Sidorov <roman.sidorov@icon.team>
2024-09-27 15:54:36 +1000

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  'https://utfs.io/f/Gj6Se8vrspT3qgLkcF3dylueAsCvYn07LDzkZfqU1bNoPVtw',
+  'https://utfs.io/f/Gj6Se8vrspT3qgLkcF3dylueAsCvYn07LDzkZfqU1bNoPVtw',
+  'https://utfs.io/f/Gj6Se8vrspT3qgLkcF3dylueAsCvYn07LDzkZfqU1bNoPVtw',
+  'https://utfs.io/f/Gj6Se8vrspT3qgLkcF3dylueAsCvYn07LDzkZfqU1bNoPVtw'
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
